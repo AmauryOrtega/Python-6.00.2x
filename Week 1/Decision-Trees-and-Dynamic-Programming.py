8e7de41ab735e2290b22b6921f71b009546c9f37
Amaury Ortega <amauryocortega@gmail.com>
2017-03-29 15:13:30 -0500

    evitar borrado de archivos

--- Week 1/Decision-Trees-and-Dynamic-Programming.py
+++ Week 1/Decision-Trees-and-Dynamic-Programming.py
@@ -8,10 +8,10 @@ Created on 25/03/2017
 """
 **Search tree**
 
+Left first, depth first enumeration
+
 Take an element from the still to be considered items
 if there is room for that item, a node is made that represents that choice, that will be the left node
 always check the consequences of not taking the item, that will be the right node
 This has to be recursively called to non-leaf nodes
 """
-
-
